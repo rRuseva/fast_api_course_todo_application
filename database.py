0a48acc5aa49f3c4ae41dce1e00e0f53a220ffb3
Radina <ruseva.radina@gmail.com>
2026-08-23 19:21:16 +0300

    Revert back to Sqlite3

--- database.py
+++ database.py
@@ -3,17 +3,17 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # SQLite3 connection
-# SQL_ALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
-# engine = create_engine(SQL_ALCHEMY_DATABASE_URL,
-#                        connect_args={"check_same_thread": False})
+SQL_ALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
+engine = create_engine(SQL_ALCHEMY_DATABASE_URL,
+                       connect_args={"check_same_thread": False})
 
 # PostgreSQL connection
 # SQL_ALCHEMY_DATABASE_URL = "postgresql://postgres:admin@localhost/TodoApplicationDatabase"
 # engine = create_engine(SQL_ALCHEMY_DATABASE_URL)
 
-# MySql connection
-SQL_ALCHEMY_DATABASE_URL = "mysql+pymysql://root:admin@localhost:3306/TodoApplicationDatabase"
-engine = create_engine(SQL_ALCHEMY_DATABASE_URL)
+# # MySql connection
+# SQL_ALCHEMY_DATABASE_URL = "mysql+pymysql://root:admin@localhost:3306/TodoApplicationDatabase"
+# engine = create_engine(SQL_ALCHEMY_DATABASE_URL)
 
 
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind=engine)
