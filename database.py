563b6b91b6bc0ff866c0f0c6c9a073a1dc50503f
Radina <ruseva.radina@gmail.com>
2026-08-23 17:19:05 +0300

    Todos app version 2 implemented with Postgresql

--- database.py
+++ database.py
@@ -2,10 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQL_ALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
+# SQLite3 connection
+# SQL_ALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
+# engine = create_engine(SQL_ALCHEMY_DATABASE_URL,
+#                        connect_args={"check_same_thread": False})
 
-engine = create_engine(SQL_ALCHEMY_DATABASE_URL,
-                       connect_args={"check_same_thread": False})
+# PostgreSQL connection
+SQL_ALCHEMY_DATABASE_URL = "postgresql://postgres:admin@localhost/TodoApplicationDatabase"
+
+engine = create_engine(SQL_ALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind=engine)
 
